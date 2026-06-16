efe5f0afdbe11cc45422490508b1d09ecec1aa98
YashvanthRaam7 <yashvanthraam55@gmail.com>
2026-06-16 11:04:36 +0530

    Update app run configuration for production
    Changed the default port from 5000 to 7860 and set debug to False.

--- Assignment 1 full stack web/backend/app.py
+++ Assignment 1 full stack web/backend/app.py
@@ -201,4 +201,5 @@ def catch_all(path):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=not IS_PROD)
+    port = int(os.environ.get("PORT", 7860))
+    app.run(host="0.0.0.0", port=port, debug=False)
